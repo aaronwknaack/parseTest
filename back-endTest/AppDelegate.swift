46b63c4df54bccd22a50ad996c987c1b84ea9d4e
aaronwknaack <wurtingerkna@wisc.edu>
2015-11-05 19:01:55 -0600

    Added our Parse Back-end

--- back-endTest/AppDelegate.swift
+++ back-endTest/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("ePRS1kvzndom5s2d3WGSN42LITrZaghQMO8tgd5J",
+            clientKey: "TBr2EaprCkXVoZTz1RzGVrYEuELb4RmaDaZkDnpZ")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         // Override point for customization after application launch.
         return true
     }
--- back-endTest/ViewController.swift
+++ back-endTest/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
+    
+    //Created array variables to store all data from each object in the trivia class from back-end
+    var question:[String] = [String]();
+    var ans2:[String] = [String]();
+    var ans3:[String] = [String]();
+    var correctAns:[String] = [String]();
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   override func viewDidLoad() {
+       super.viewDidLoad()
+    
+    //Retrieve trivia objects from Parse
+    self.retrieveTrivia()
+    
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+       super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+        func retrieveTrivia() {
+            
+            //create new pfQuery - This is the bridge between our app and Parse: "trivia" is our class name on Parse
+            let queryTrivia:PFQuery = PFQuery(className:"trivia")
+            
+            //This method gives access to all objects in "trivia" class using our queryTrivia Bridge
+            queryTrivia.findObjectsInBackgroundWithBlock { (objects:[PFObject]?, error:NSError?) -> Void in
+                
+                // Loop through the objects array
+                for triviaObject in objects!{
+                    
+                    // Retrieve data for each object (question, ans2, ans3, correctAns)
+                    let triviaQuestion:String? = (triviaObject as PFObject)["question"] as? String
+                    let triviaAns2:String? = (triviaObject as PFObject)["ans2"] as? String
+                    let triviaAns3:String? = (triviaObject as PFObject)["ans3"] as? String
+                    let triviaAns:String? = (triviaObject as PFObject)["correctAns"] as? String
+                    
+                    // Assign to arrays
+                    if triviaQuestion != nil {
+                        self.question.append(triviaQuestion!)
+                    }
+                    if triviaAns2 != nil {
+                        self.ans2.append(triviaAns2!)
+                    }
+                    if triviaAns3 != nil {
+                        self.ans3.append(triviaAns3!)
+                    }
+                    if triviaAns != nil {
+                        self.correctAns.append(triviaAns!)
+                    }
+                    
+                }
+                
+                
+                //This is to check that data has been succesfully transfered
+                //Print all Data in order stored in our question array
+                print("Questions in order as they are stored:")
+                for element in self.question{
+                    print(element)
+                }
+                
+                print(" ")
+                print("Correct Ansers in order as they are stored:")
+                
+                //Print all Data in order stored in our correctAns array
+                for element in self.correctAns{
+                    print(element)
+                }
+                //The first of the list printed from question should correspond to the
+                //first item in the second list, and the 2nd match to the 2nd, so forth.
+                
+            }
+            
+        }
+
+
 
 }
-
